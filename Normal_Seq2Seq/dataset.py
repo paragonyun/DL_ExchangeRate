2ec6a549fe0c92b44e73138ee07c56cbbba342e6
paragonyun <paragonyun@naver.com>
2023-01-15 12:22:35 +0900

    feat: make it possible to check scaler

--- Normal_Seq2Seq/dataset.py
+++ Normal_Seq2Seq/dataset.py
@@ -12,34 +12,35 @@ class NormalSeq2SeqDataset(Dataset):
     Input Window와 output Window를 설정하여 해당 기간들이 담긴 데이터셋을 생성해야합니다.
 
     ex) [1,2,3,4,5,6,7,8,9,10] , IW = 3, OW = 1, Stride = 1
-    Inputs -> [1,2,3] , [2,3,4], [3,4,5], [4,5,6], [5,6,7], [6,7,8], [7,8,9] 
+    Inputs -> [1,2,3] , [2,3,4], [3,4,5], [4,5,6], [5,6,7], [6,7,8], [7,8,9]
     Outputs -> [4], [5], [6], [7], [8], [9], [10]
     num_samples = Lenth - IW - OW // (stride + 1)
                 = ((10 - 3 - 1) // 1 ) + 1
                 = 6//1 + 1 = 7개
     """
+
     def __init__(self, df, IW=14, OW=7, stride=1):
         # 전처리
-        prep_df, fitted_mm = return_prep_data(df)
+        prep_df, self.fitted_mm = return_prep_data(df)
 
         Length = df.shape[0]
 
         num_samples = ((Length - IW - OW) // stride) + 1
 
         Xs = np.zeros([IW, num_samples])
-        Ys= np.zeros([OW, num_samples])
+        Ys = np.zeros([OW, num_samples])
 
-        for i in range(num_samples): # Samples를 만듭니다.
-            x_start = stride * i 
+        for i in range(num_samples):  # Samples를 만듭니다.
+            x_start = stride * i
             x_end = x_start + IW
-            Xs[:, i] = prep_df['rate'][x_start:x_end]
+            Xs[:, i] = prep_df["rate"][x_start:x_end]
 
-            y_start = stride * i + IW 
+            y_start = stride * i + IW
             y_end = y_start + OW
-            Ys[:, i] = prep_df['rate'][y_start:y_end]
+            Ys[:, i] = prep_df["rate"][y_start:y_end]
 
-        Xs = Xs.reshape(Xs.shape[0], Xs.shape[1], 1).transpose((1,0,2))
-        Ys = Ys.reshape(Ys.shape[0], Ys.shape[1], 1).transpose((1,0,2))
+        Xs = Xs.reshape(Xs.shape[0], Xs.shape[1], 1).transpose((1, 0, 2))
+        Ys = Ys.reshape(Ys.shape[0], Ys.shape[1], 1).transpose((1, 0, 2))
 
         self.Xs = Xs
         self.Ys = Ys
