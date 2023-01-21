e4b652f1a8ff5382d002d0f1d4ed31eb8322c5b6
paragonyun <paragonyun@naver.com>
2023-01-21 13:50:36 +0900

    Rethinking of Attention Seq2Seq

--- Attention_Seq2Seq/model.py
+++ Attention_Seq2Seq/model.py
@@ -52,30 +52,40 @@ class Decoder(nn.Module):
 
         self.encoder_weight = nn.Linear(in_features=self.hidden_size, out_features=self.hidden_size, bias=False)
         self.decoder_weight = nn.Linear(in_features=self.hidden_size, out_features=self.hidden_size, bias=False)
-        self.value_weight = nn.Linear(in_features=self.hidden_size, out_features=14, bias=False)
+        self.value_weight = nn.Linear(in_features=self.hidden_size, out_features=1, bias=False)
 
-        self.fin_linear = nn.Linear(in_features=self.hidden_size + 14, out_features=1)
+        self.fin_linear = nn.Linear(in_features=self.hidden_size, out_features=self.input_size)
 
-    def forward(self, x, encoder_input_hidden_state):
-        
-        lstm_output, self.hidden = self.lstm(
-            x.unsqueeze(-1), encoder_input_hidden_state
-        )
-
+    def forward(self, x, hidden, encoder_output):
         ## hidden state활용
+        # print("input size: ", x.size()) # 32, 1
+        # print("encoder output: ", encoder_output.size()) # 32, 14, 64
+        # print("hidden[0] size: ", hidden[0].size())
         attn_scores = self.value_weight(
             torch.tanh(
-                self.encoder_weight(encoder_input_hidden_state[0].permute(1, 0, 2)) + 
-                self.decoder_weight(self.hidden[0].permute(1, 0, 2))
+                self.encoder_weight(encoder_output) + 
+                self.decoder_weight(hidden[0].permute(1, 0, 2))
             )
-        )
-        attn_weight = torch.softmax(attn_scores, dim=2)
-
-        context_vector = torch.cat((attn_weight, lstm_output), dim=2).squeeze(1)
-
-        output = self.fin_linear(context_vector)
+        ) 
+        # print(attn_scores.size()) # 32, 14, 1
+        attn_weight = torch.softmax(attn_scores, dim=1) # 32, 14, 1
+        # print(attn_weight.permute(0, 2, 1).size())
         
-        return output, self.hidden, attn_weight
+        context_vector = torch.bmm(attn_weight.permute(0, 2, 1), encoder_output)
+        # print("CV : ", context_vector.size()) # 32, 1, 64
+
+        new_input = torch.cat((context_vector, x.unsqueeze(1)), dim=2)
+
+        new_input = new_input.permute(0, 2, 1)
+        # print("new input size: ", new_input.size()) # 32, 65, 1
+
+        output, hidden = self.lstm(new_input, hidden) 
+
+        # print("output Size : ", output[:, -1, :].size()) # 32, 64
+
+        fin_output = self.fin_linear(output[:, -1, :])
+
+        return fin_output, hidden, attn_weight
 
 
 class AttentionSeq2SeqModel(nn.Module):
@@ -95,21 +105,25 @@ class AttentionSeq2SeqModel(nn.Module):
 
         outputs = torch.zeros(bs, target_len, input_size)
 
-        _, hidden_state = self.encoder(inputs)
+        encoder_output, hidden_state = self.encoder(inputs)
 
         decoder_input = inputs[:, -1, :]  # 최초 Decoder Input
 
+        de_hidden = hidden_state
+
         
-        total_atten_weight = torch.zeros(bs, 1, 14).to(self.device)
+        total_atten_weight = torch.zeros(bs, 14, 1).to(self.device)
         ## Decoder (예상값 출력)
         for t in range(target_len):  # OW=7이므로 7개의 out을 뱉습니다.
-            output, hidden_state, attn_weight = self.decoder(decoder_input, hidden_state)
-
+            output, de_hidden, attn_weight = self.decoder(decoder_input, hidden=de_hidden, encoder_output=encoder_output)
+            # print(output.size())
             #output = output.squeeze(1)
 
             # t시점의 output은 t+1 시점의 Input중 하나로 들어갑니다.
             decoder_input = output
-
+            # print(output[0])
+            # print(output.size())
+            
             # 결과 저장
             outputs[:, t, :] = output
 
