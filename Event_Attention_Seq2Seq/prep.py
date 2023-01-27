e29efa623ba82153183fe41946092449a5a8a1e4
paragonyun <paragonyun@naver.com>
2023-01-27 14:51:05 +0900

    Standard and Diff

--- Event_Attention_Seq2Seq/prep.py
+++ Event_Attention_Seq2Seq/prep.py
@@ -1,11 +1,40 @@
-from sklearn.preprocessing import MinMaxScaler
-
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import numpy as np
+import torch
 
 def return_prep_data(ori_df):
     print("📚 전처리를 수행합니다..")
+    print("차분 수행중...")
+    diff_rate = ori_df['rate'].diff(1)
+    ori_df = ori_df.iloc[1:, :]
+    ori_df['rate'] = diff_rate
+    first = pd.DataFrame({"Time": ["1997/12/01"],
+                        "rate": [0]})
+    ori_df = pd.concat([first, ori_df], axis=0)
+    copy_df = ori_df.copy()
 
-    mm = MinMaxScaler()
-    ori_df["rate"] = mm.fit_transform(ori_df["rate"].values.reshape(-1, 1))
+    print("Scaling 진행중...")
+    ss = StandardScaler()
+    copy_df["rate"] = ss.fit_transform(copy_df["rate"].values.reshape(-1, 1))
 
+    print("Impact Event를 찾는 중...")
+    times = ["1998/01/03", "1997/12/24", "2008/10/17", "2008/10/08", "2020/03/20", 
+            "1997/12/27", "1997/12/17", "2008/10/31", "2009/03/12", "2022/11/14" ]
+    event_tensor = find_impact_events(ori_df=ori_df, times=times)
+    
     print("✅ Done!")
-    return ori_df, mm
+    return copy_df, event_tensor, ss
+
+def find_impact_events(ori_df, times: list):
+    # time -> "year/month/day"
+    event_arr = np.array([])
+
+    for time in times:
+        idx = ori_df[ori_df['Time'] == time].index[0]
+        values = np.array(ori_df.iloc[idx-4 : idx + 3, 1])
+        event_arr = np.concatenate((event_arr, values), axis=0)
+    
+    event_arr = torch.tensor(event_arr.reshape(len(times), 7), dtype=torch.float32)
+
+    return event_arr
