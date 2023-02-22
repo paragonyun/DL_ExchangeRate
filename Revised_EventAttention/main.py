d1034951339f61a32cf3f9fce03c8577b4f6aeb1
paragonyun <paragonyun@naver.com>
2023-02-22 12:25:05 +0900

    feat: change the seq lens of training and evals

---
+++ Revised_EventAttention/main.py
@@ -0,0 +1,99 @@
+from dataloader import *
+from dataset import *
+from model import *
+from prep import *
+from utils import *
+from trainer import *
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+## Random Seed를 고정합니다.
+seed_everything(seed=43)
+
+# 전처리, 세트화가 완료된 데이터로더를 만듭니다.
+data_loader, events_mat, fitted_ss = return_dataloaders()
+
+EPOCHS = 3000
+LR = 0.001
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = AttentionSeq2SeqModel(events_mat=events_mat, input_size=1, hidden_size=64).to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=LR)
+criterion = nn.MSELoss()
+
+train = Trainer(
+    model=model,
+    loader=data_loader,
+    epoches=EPOCHS,
+    optimizer=optimizer,
+    criterion=criterion,
+    device=device,
+)
+
+atten_weights = train.doit() # test 삼아서
+
+## Evaluation
+print("✨Start Evaluation...✨")
+
+## 22년 10월 예측 - input : 22년 7,8,9월 /
+ori_df = pd.read_csv("./data/for_oct_revised_pred.csv")
+diffs = ori_df["rate"].diff()
+scaled = fitted_ss.transform(diffs.iloc[-60:].values.reshape(-1, 1))
+input_data = torch.tensor(scaled).to(device).float()
+
+model.load_state_dict(torch.load("./BEST_MODEL.pth"))
+
+predict, atten_weights, sim_scores = model.predict(inputs=input_data, target_len=20)
+
+actuals = ori_df["rate"].to_numpy()
+print("Original Prediction (Before Inverse Transform)")
+print(predict, "\n")
+
+print("Inverse Trasforming...")
+predict = fitted_ss.inverse_transform(predict.reshape(-1, 1))
+actuals = fitted_ss.inverse_transform(actuals.reshape(-1, 1))
+print("Inversed Changes\n", predict)
+
+predictions = change_to_original(df=ori_df, preds=predict)
+
+print("\nAttention Weights")
+print(atten_weights,"\n")
+print("\nEvent Similarity (Check the prep.py's events)")
+print(sim_scores, "\n\n")
+
+print("👀Prediction👀")
+print(predictions)
+
+
+## 23년 1월 예측 - input : 22년 10,11,12월 
+ori_df = pd.read_csv("./data/for_23y_revised_pred.csv")
+diffs = ori_df["rate"].diff()
+scaled = fitted_ss.transform(diffs.iloc[-60:].values.reshape(-1, 1))
+input_data = torch.tensor(scaled).to(device).float()
+
+model.load_state_dict(torch.load("./BEST_MODEL.pth"))
+
+predict, atten_weights, sim_scores = model.predict(inputs=input_data, target_len=20)
+
+actuals = ori_df["rate"].to_numpy()
+print("Original Prediction (Before Inverse Transform)")
+print(predict, "\n")
+
+print("Inverse Trasforming...")
+predict = fitted_ss.inverse_transform(predict.reshape(-1, 1))
+actuals = fitted_ss.inverse_transform(actuals.reshape(-1, 1))
+print("Inversed Changes\n", predict)
+
+predictions = change_to_original(df=ori_df, preds=predict)
+
+print("\nAttention Weights")
+print(atten_weights,"\n")
+print("\nEvent Similarity (Check the prep.py's events)")
+print(sim_scores, "\n\n")
+
+print("👀Prediction👀")
+print(predictions)
---
+++ Revised_EventAttention/prep.py
@@ -0,0 +1,40 @@
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import numpy as np
+import torch
+
+def return_prep_data(ori_df):
+    print("📚 전처리를 수행합니다..")
+    print("차분 수행중...")
+    diff_rate = ori_df['rate'].diff(1)
+    ori_df = ori_df.iloc[1:, :]
+    ori_df['rate'] = diff_rate
+    first = pd.DataFrame({"Time": ["1997/12/01"],
+                        "rate": [0]})
+    ori_df = pd.concat([first, ori_df], axis=0)
+    copy_df = ori_df.copy()
+
+    print("Scaling 진행중...")
+    ss = StandardScaler()
+    copy_df["rate"] = ss.fit_transform(copy_df["rate"].values.reshape(-1, 1))
+
+    print("Impact Event를 찾는 중...")
+    times = ["1998/11/24", "1998/01/03", "2020/12/07", "2008/10/08", "2008/08/27", 
+            "2013/04/12", "2010/06/30", "2008/04/22", "2014/03/07", "2014/08/21" ]
+    event_tensor = find_impact_events(ori_df=ori_df, times=times)
+    
+    print("✅ Done!")
+    return copy_df, event_tensor, ss
+
+def find_impact_events(ori_df, times: list):
+    # time -> "year/month/day"
+    event_arr = np.array([])
+
+    for time in times:
+        idx = ori_df[ori_df['Time'] == time].index[0]
+        values = np.array(ori_df.iloc[idx-4 : idx + 3, 1])
+        event_arr = np.concatenate((event_arr, values), axis=0)
+    
+    event_arr = torch.tensor(event_arr.reshape(len(times), 7), dtype=torch.float32)
+
+    return event_arr
