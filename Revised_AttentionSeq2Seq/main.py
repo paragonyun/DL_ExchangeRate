4fa5df340c45472d69049425a0a16fd3d7e73909
paragonyun <paragonyun@naver.com>
2023-02-24 19:18:05 +0900

    Fix Typo

--- Revised_AttentionSeq2Seq/main.py
+++ Revised_AttentionSeq2Seq/main.py
@@ -20,7 +20,7 @@ LR = 0.001
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-model = AttentionSeq2SeqModel(events_mat=events_mat, input_size=1, hidden_size=64).to(device)
+model = AttentionSeq2SeqModel(input_size=1, hidden_size=64).to(device)
 
 optimizer = optim.Adam(model.parameters(), lr=LR)
 criterion = nn.MSELoss()
