a4c8c030e5382c3e8c1b6c3365f68de9b2730f90
paragonyun <paragonyun@naver.com>
2023-02-24 19:12:02 +0900

    Revised Attention Seq2Seq

---
+++ Revised_AttentionSeq2Seq/model.py
@@ -0,0 +1,171 @@
+import torch
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    """Cell은 LSTM으로 했습니다."""
+
+    def __init__(
+        self,
+        input_size,
+        hidden_size,
+        num_layers=1,
+    ):
+        super(Encoder, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+        )
+
+    def forward(self, x):
+        lstm_output, self.hidden = self.lstm(x)
+
+        return lstm_output, self.hidden
+
+
+class Decoder(nn.Module):
+    def __init__(
+        self,
+        input_size,
+        hidden_size,
+        num_layers=1,
+    ):
+        super(Decoder, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+        )
+
+        self.encoder_weight = nn.Linear(in_features=self.hidden_size, out_features=self.hidden_size, bias=False)
+        self.decoder_weight = nn.Linear(in_features=self.hidden_size, out_features=self.hidden_size, bias=False)
+        self.value_weight = nn.Linear(in_features=self.hidden_size, out_features=1, bias=False)
+
+        self.fin_linear = nn.Linear(in_features=self.hidden_size, out_features=self.input_size)
+
+    def forward(self, x, hidden, encoder_output):
+        bs = x.size(0)
+        ## hidden state활용
+        # print("input size: ", x.size()) # 32, 1
+        # print("encoder output: ", encoder_output.size()) # 32, 14, 64
+        # print("hidden[0] size: ", hidden[0].size()) # 1, 32, 64
+        # print(self.encoder_weight(encoder_output).size())
+        # print(self.decoder_weight(hidden[0].permute(1, 0, 2)).size())
+        # print((self.encoder_weight(encoder_output) + self.decoder_weight(hidden[0].permute(1, 0, 2))).size() )
+        attn_scores = self.value_weight(
+            torch.tanh(
+                self.encoder_weight(encoder_output) + 
+                self.decoder_weight(hidden[0].permute(1, 0, 2))
+            )
+        ).squeeze(2) 
+        # print(attn_scores.size()) # 32, 14
+        attn_weight = torch.softmax(attn_scores, dim=1)
+
+        # print(attn_weight.permute(0, 2, 1).size())
+        # print(attn_weight.size())
+        context_vector = torch.bmm(attn_weight.unsqueeze(1), encoder_output).squeeze(1)
+        # print("CV : ", context_vector.size()) # 32, 64
+
+        new_input = torch.cat((context_vector, x), dim=1).unsqueeze(-1)
+
+        # new_input = new_input.permute(0, 2, 1)
+        # print("new input size: ", new_input.size()) # 32, 65, 1
+
+        _, hidden = self.lstm(new_input, hidden) 
+
+        # print(output.size()) # 32, 65, 64
+        # print("output Size : ", output.size()) # 32, 64
+        # print("hidden Size : ", hidden[0].size()) # 1, 32, 64
+        # print("Last Hidden : ", hidden[0][-1].size()) # 32, 64 오!! 이거네
+
+        fin_output = self.fin_linear(hidden[0][-1])
+        # print("Final Ouptut Size : ", fin_output.size()) # 32, 1
+
+        return fin_output, hidden, attn_weight
+
+
+class AttentionSeq2SeqModel(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(AttentionSeq2SeqModel, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+
+        self.encoder = Encoder(input_size=input_size, hidden_size=hidden_size)
+        self.decoder = Decoder(input_size=input_size, hidden_size=hidden_size)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    def forward(self, inputs, target_len):  # X  # OW
+        bs = inputs.shape[0]
+        input_size = inputs.shape[2]
+
+        outputs = torch.zeros(bs, target_len, input_size)
+
+        encoder_output, hidden_state = self.encoder(inputs)
+
+        decoder_input = inputs[:, -1, :]  # 최초 Decoder Input
+
+        de_hidden = hidden_state
+
+        total_atten_weight = torch.zeros(bs, 60).to(self.device)
+        ## Decoder (예상값 출력)
+        for t in range(target_len):  # OW=7이므로 7개의 out을 뱉습니다.
+            output, de_hidden, attn_weight = self.decoder(decoder_input, hidden=de_hidden, encoder_output=encoder_output)
+
+            # output = output.squeeze(1)
+
+
+            # t시점의 output은 t+1 시점의 Input중 하나로 들어갑니다.
+            decoder_input = output
+            # print(output[0])
+            # print(output.size())
+            
+            # 결과 저장
+            outputs[:, t, :] = output
+
+            # 종합적인 attn weight를 확인
+            total_atten_weight += attn_weight
+
+        return outputs, total_atten_weight
+
+    def predict(self, inputs, target_len):
+        self.eval()  # Inference Mode
+
+        inputs = inputs.unsqueeze(0)
+        bs = inputs.shape[0]
+        input_size = inputs.shape[2]  # 7이 될 겁니다.
+
+        outputs = torch.zeros(bs, target_len, input_size)
+
+        encoder_output, hidden_state = self.encoder(inputs)
+        decoder_input = inputs[:, -1, :]
+
+        de_hidden = hidden_state
+
+        total_attn_weight = torch.zeros(bs, 60).to(self.device)
+
+        for t in range(target_len):
+            output, de_hidden, attn_weight = self.decoder(decoder_input, hidden=de_hidden, encoder_output=encoder_output)
+
+            # output = output.squeeze(1)
+
+            decoder_input = output
+
+            outputs[:, t, :] = output
+            
+            total_attn_weight += attn_weight
+
+        return outputs.detach().numpy()[0, :, 0], total_attn_weight
---
+++ Revised_AttentionSeq2Seq/trainer.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+
+from tqdm import tqdm
+import pickle
+
+
+class Trainer:
+    def __init__(
+        self,
+        model,
+        loader,
+        epoches,
+        optimizer,
+        criterion,
+        device="cuda" if torch.cuda.is_available() else "cpu",
+    ):
+        self.model = model
+        self.loader = loader
+        self.epoches = epoches
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.device = device
+
+    def doit(self):
+        print("🚀Start Training...🚀")
+        print(f"Using Resource : {self.device}")
+        self.model.train()
+
+        best_loss = 10000
+
+        total_atten_weights = torch.zeros(32, 60).to(self.device)
+
+        for epoch in range(1, self.epoches + 1):
+            running_loss = 0.0
+            epoch_atten_weights = torch.zeros(32, 60).to(self.device)
+            for x, y in tqdm(self.loader):
+                x, y = x.to(self.device).float(), y.to(self.device).float()
+
+                self.optimizer.zero_grad()
+                # torch.autograd.set_detect_anomaly(True)
+                
+                preds, atten_weights = self.model(inputs=x, target_len=20)#.to(self.device)  # OW가 들어갑니다
+
+                preds = preds.to(self.device)
+
+                loss = self.criterion(preds, y)
+
+                loss.backward()
+
+                self.optimizer.step()
+
+                running_loss += loss.item()
+                
+                if atten_weights.size(0) == 22:
+                    temp_tensor = torch.zeros(10, 60).to(self.device)
+                    atten_weights = torch.cat((atten_weights, temp_tensor), dim=0)
+
+                epoch_atten_weights += atten_weights
+
+            total_atten_weights += epoch_atten_weights
+
+            total_atten_weights_dist = torch.softmax(total_atten_weights, dim=1)
+
+            train_loss = running_loss / len(self.loader)
+
+            print("======================================================================")
+            print(f"EPOCH [{epoch}/{self.epoches}]")
+            print(f"TOTAL LOSS : {running_loss:4f}\tAVG LOSS : {train_loss:.4f}")
+
+            if epoch % 100 == 0:
+                print("Attention Weights Distributions")
+                print(total_atten_weights)
+                print(total_atten_weights_dist)
+                with open(f"./Attention_Weights_ep{epoch}.pkl", "wb") as f:
+                    pickle.dump(total_atten_weights, f)
+
+                with open(f"./Attention_Distribution_ep{epoch}.pkl", "wb") as f:
+                    pickle.dump(total_atten_weights_dist, f)
+                
+                print("👍 Attention Weight and Distribusion is Saved")
+
+                del total_atten_weights
+                del total_atten_weights_dist
+                total_atten_weights = torch.zeros(32, 60).to(self.device)
+
+            if running_loss < best_loss:
+                print("🚩 Saving Best Model...")
+                torch.save(self.model.state_dict(), "./BEST_MODEL.pth")
+                best_loss = running_loss
+
+        print("📃 Save the Trained Model for Prediction...")
+        torch.save(self.model.state_dict(), "./Final_Model.pth")
+        print("✅ Done!")
+
+        return total_atten_weights
